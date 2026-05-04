93cfc98c2dc7893720464b5a500cb8efbbc700da
htngntks <25021750@vnu.edu.vn>
2026-05-04 12:15:33 +0700

    Add JavaDoc comments

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,10 +1,16 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Calculator class.
+ */
 public class Calculator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Calculator.class);
 
+    /**
+     * Add 2 integer numbers.
+     */
     public int add(int a, int b) {
         int result = a + b;
         LOGGER.info("In add() method: a={}, b={}, result={}", a, b, result);
