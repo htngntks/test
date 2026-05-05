2d7c72064b92fc731dd74db68665d0d9bb197118
htngntks <25021750@vnu.edu.vn>
2026-05-05 23:12:04 +0700

    Cố tình tạo một Unit Test sử dụng định dạng cứng của Linux

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,10 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class CalculatorTest {
     @Test
@@ -8,7 +12,7 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
         int actualResult = calculator.add(2, 3);
         int expectedResult = 5;
-        Assertions.assertEquals(expectedResult, actualResult);
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
@@ -16,12 +20,18 @@ public class CalculatorTest {
         Calculator calculator = new Calculator();
         int actualResult = calculator.divide(6, 3);
         int expectedResult = 2;
-        Assertions.assertEquals(expectedResult, actualResult);
+        assertEquals(expectedResult, actualResult);
     }
 
     @Test
     public void testDivide2() {
         Calculator calculator = new Calculator();
         Assertions.assertThrows(IllegalArgumentException.class, () -> calculator.divide(3, 0));
+    }
+    @Test
+    public void testFilePath() {
+        String expectedPath = "logs/app.log";
+        File logFile = new File("logs", "app.log");
+        assertEquals(expectedPath, logFile.getPath());
     }
 }
